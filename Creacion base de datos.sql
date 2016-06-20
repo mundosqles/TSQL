1a99cac88482478d6b7cc0b0689d3317c307c0ab
mundosqles <mundosql@outlook.es>
2016-06-20 12:50:36 +0200

    creacion base datos

---
+++ Creacion base de datos.sql
@@ -0,0 +1,23 @@
+﻿/* CREACION DE BASE DE DATOS */
+/* http://www.mundosql.es/ */
+CREATE DATABASE AGENDA
+ON
+/* DONDE GUARDAMOS DATOS*/
+( NAME = AGENDA,
+    FILENAME = 'M:\Proyectos\SQL Server - Agenda\Agenda.MDF',
+    SIZE = 10,
+    MAXSIZE = 50,
+    FILEGROWTH = 5 )
+/* DONDE GUARDAMOS LOS LOGS*/
+LOG ON
+( NAME = AGENDA_LOG,
+    FILENAME = 'M:\Proyectos\SQL Server - Agenda\Agenda.LDF',
+    SIZE = 5MB,
+    MAXSIZE = 25MB,
+    FILEGROWTH = 5MB ) ;
+/*
+FILENAME = ombre fichero
+SIZE = TamañO del archivo
+MAXSIZE = Tamaño máximo que puede alcanzar el archivo
+FILEGROWTH = Incremento de crecimiento automático del archivo
+*/
