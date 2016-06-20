8afa528bcd1bee89a005d6dbbeb6a89f39c7ab07
mundosqles <mundosql@outlook.es>
2016-06-20 17:05:07 +0200

    cambio credencial

--- Credendial - Creacion.sql
+++ Credendial - Creacion.sql
@@ -1,5 +1,5 @@
 /* CREAD CREDENCIAL AL BLOLB BBDD*/
 /* http://www.mundosql.es/ */
 
-CREATE CREDENTIAL conexalmacbbdd WITH IDENTITY = 'almacbbdd',   
-    SECRET = 'pBd4UhgASWqYGGjstAuOoH2Tz/VhIsdATIKoDsBzvSxfJ7TjTOEz/yvfTaI8JM+R+87M276EBzhZ0qmzhGX0BA==';  
+CREATE CREDENTIAL nombreconexion WITH IDENTITY = 'almacenamiento',   
+    SECRET = 'clave a utilizar';  
